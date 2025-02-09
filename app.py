a3f339c6e67e27e9edd62cf66b8a057d514c6894
Samuel Santos <samuka0102128@gmail.com>
2025-02-09 12:50:32 -0300

    Setup user and authentication

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models.user import User
+from flask_login import LoginManager, login_user, current_user
 
 
 app = Flask(__name__)
@@ -8,11 +9,34 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
 
-@app.route("/hello-world", methods=["GET"])
-def Hello_world():
-    return "Hello, World!"
+#View de Login 
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        #Login
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Credenciais enviadas com sucesso!"})
+
+        return jsonify({"message": "Credenciais inválidas"}), 400
 
 
 if __name__ == "__main__":
