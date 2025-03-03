6eab101e1c8f8878d9d63860e93fcc7778e9555e
Samuel Santos <samuka0102128@gmail.com>
2025-03-02 23:32:20 -0300

    DELETE user

--- app.py
+++ app.py
@@ -81,5 +81,19 @@ def update_user(id_user):
         return jsonify({"message": f"Usuário {id_user} foi atualizado com sucesso!"})
     return jsonify({"message": "Usuário não encontrado."}), 404
 
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required 
+def delete_user(id_user):
+    user = User.query.get(id_user)
+    
+    if id_user == current_user.id:
+        return jsonify({"message": "Deleção não permitida"}), 403
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário {id_user} foi deletado."})
+    return jsonify({"message": "Usuário não encontrado."}), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
